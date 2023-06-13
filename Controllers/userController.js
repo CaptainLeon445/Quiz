102e8238d5f359b0305406ae4336b23097c2f55c
AFRIKAN BACKEND DEVELOPER <christopherleon237@gmail.com>
2023-06-13 15:22:22 +0100

    jwt authentication added to the create user endpoint

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,123 +1,133 @@
-const User=require("./../Models/userModel")
-const mongoose=require("mongoose")
+const jwt = require("jsonwebtoken");
+const User = require("./../Models/userModel");
+const mongoose = require("mongoose");
 
 // create user
-exports.createUser=async(req, res)=>{
-    const doc=await User.create(req.body)
-    try{
-        res.status(200).json({
-            message:"success",
-            data: doc
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err
-        })
-    }
-}
+exports.createUser = async (req, res) => {
+  const doc = await User.create({
+                                    username: req.body.username,
+                                    email: req.body.email,
+                                    password: req.body.password,
+                                    passwordConfirm: req.body.passwordConfirm,
+                                });
+  const token = jwt.sign({id:doc._id}, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
+  try {
+    res.status(200).json({
+      message: "success",
+      token,
+      data: doc,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err,
+    });
+  }
+};
 
 // get all the users
-exports.getAllUsers=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.getAllUsers = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // get a user
-exports.getAUser=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.getAUser = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // update the user
-exports.updateUser=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.updateUser = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // delete the user
-exports.deleteUser=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.deleteUser = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // get me
-exports.getMe=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.getMe = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // update me
-exports.updateMe=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.updateMe = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
 
 // delete me
-exports.deleteMe=async(req, res)=>{
-    try{
-        res.status(200).json({
-            message:"success",
-            data:"Route is yet to be defined"
-        })
-    }catch(err){
-        res.status(400).json({
-            message:"fail",
-            err: err.message
-        })
-    }
-}
+exports.deleteMe = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "success",
+      data: "Route is yet to be defined",
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "fail",
+      err: err.message,
+    });
+  }
+};
--- Models/userModel.js
+++ Models/userModel.js
@@ -2,8 +2,6 @@ const mongoose=require("mongoose")
 const validator=require("validator")
 const bcrypt=require("bcryptjs")
 
-
-
 const userSchema=mongoose.Schema({
     username:{
         type:String,
